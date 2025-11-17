74dfad16cd3f929bb2a661a2c5cfefa4a19dbdfb
Pakorn Fudulyawajananont <pakornf@Pakorns-MacBook-Air-2.local>
2025-11-17 14:26:40 +0700

    Expression of this course

--- app.py
+++ app.py
@@ -4,7 +4,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Hello, Docker World!"
+    return "Hello, My name is Fong! I'm studying ISP and wanted to share my excitement about it with you." \
+    "This course has been incredibly engaging and informative, and I'm thrilled to be a part of it." \
+    "This course also gives me an oppotunity to simulate real world projects which is very helpful for my future career." \
+    "Looking forward to learning more and collaborating with everyone!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5010)
